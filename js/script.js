3ce589424a514472bd3e1e4155e52258992909e3
mileto1975 <mileto.di.marco@howest.be>
2020-03-09 15:28:15 +0100

    First commit

---
+++ js/script.js
@@ -0,0 +1,28 @@
+"use strict";
+
+//declare global vars here
+ var btnSearch;
+ var btnCut;
+ var txtHaystack;
+ var txtNeedle
+ var txtCut;
+ var divResult;
+window.addEventListener('load',Initialize);
+
+function Initialize()
+{
+   //bind HTML elements
+   BindHTMLElements();
+
+   //add eventlisteners
+   AddEventListeners();
+   
+}
+
+
+
+
+
+
+
+
